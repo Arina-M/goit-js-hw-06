84b2255ad02509a7095ea0d6dc263dbf58523157
Arina Miahkykh <79450238+Arina-M@users.noreply.github.com>
2023-04-13 11:54:32 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+    if(email === '' || password === ''){
+        alert('Fill in all fields, please!');
+    }
+    
+    const formData = {
+        email,
+        password,
+    };
+
+    console.log(formData);
+    event.currentTarget.reset();
+});
