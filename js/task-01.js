120c4e0b8ee36ca33464b2b0a4a0e53e9ecd2bc6
Arina Miahkykh <79450238+Arina-M@users.noreply.github.com>
2023-04-13 10:07:20 +0300

    homework_6

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const li = document.querySelector('#categories').children.length;
+//console.log(li);
+//let lisss = li.children.length;
+console.log(`Number of categories: ${li}`);
+
+let item = document.querySelector('.item').firstElementChild;
+
+console.log(item);
+
