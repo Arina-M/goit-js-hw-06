b91fc62ba8734dcb80567d2f40e9bb8be36bdfa3
Arina Miahkykh <79450238+Arina-M@users.noreply.github.com>
2023-04-13 11:56:39 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonChangeBG = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+
+buttonChangeBG.addEventListener('click', () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  textColor.innerText = document.body.style.backgroundColor;
+});
