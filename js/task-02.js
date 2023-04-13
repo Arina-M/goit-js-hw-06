424926b21c0bb371bb4026653a65317037f36019
Arina Miahkykh <79450238+Arina-M@users.noreply.github.com>
2023-04-13 10:40:20 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('ul#ingredients');
+const makeItem = ingredients => {
+  return ingredients.map(ingredient => {
+    const item = document.createElement('li');
+    item.classList.add('item');
+    item.textContent = ingredient;
+    return item;
+  });
+}
+const elements = makeItem(ingredients);
+ingredientsList.append(...elements);
